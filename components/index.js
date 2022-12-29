a8e6e7ef091bba545b934b2fe5e9cf4c7c37b624
Tymek <tymek.morawiec@gmail.com>
2022-12-29 23:31:06 +0100

    done

--- components/index.js
+++ components/index.js
@@ -1,5 +1,9 @@
 const characterReadMore = document.querySelectorAll(".character__button");
-
+const navbarList = document
+  .querySelector(".navbar__list")
+  .querySelectorAll("li");
+const sections = document.querySelectorAll("section");
+console.log(sections);
 characterReadMore.forEach((element) => {
   element.addEventListener("click", (event) => {
     event.target.closest("div").querySelector("p").classList.toggle("hidden");
@@ -10,3 +14,9 @@ characterReadMore.forEach((element) => {
     }
   });
 });
+
+for (let i = 0; i < sections.length; i++) {
+  navbarList[i].addEventListener("click", () => {
+    sections[i].scrollIntoView({ behavior: "smooth", block: "start" });
+  });
+}
